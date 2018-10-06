bcb1d14dae7076936191ea985696b1237cac1f9c
Khaled <grira.khaled@outlook.fr>
2018-10-06 16:51:42 +0200

    ok2

--- progcombatmdr.py
+++ progcombatmdr.py
@@ -10,22 +10,24 @@ def avancer(k=50):
 def reculer(k=50):
   avancer(-k)
 
-def turnleft(k=50):
-  g.turn(k)
-  d.turn(k)
-  sleep(0.5)
-  g.turn(0)
-  d.turn(0)
-
-def turnright(k=50):
+def turnleft(k=70):
   g.turn(-k)
   d.turn(-k)
   sleep(0.5)
   g.turn(0)
   d.turn(0)
 
+def turnright(k=70):
+  g.turn(k)
+  d.turn(k)
+  sleep(0.5)
+  g.turn(0)
+  d.turn(0)
+
 def moulin(k=70):
     m.turn(k)
+def stopmoulin(k=70):
+    m.turn(0)
 
 def arrete():
     g.turn(0)
